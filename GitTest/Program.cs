d9ab138596fb34da1361813aa6426ab433dca3d9
Mark Wang <Mark.Wang@quest.com>
2020-06-12 16:39:25 +0800

    New class with nullable module

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace GitTest
 {
+    class NullableTest
+    {
+        public void ShowNumber()
+        {
+            int? i = null;
+            double? d = null;
+
+            int j = 15;
+            double f = 3.1415;
+
+            char? s = null;
+            bool? b = null;
+
+            bool p = true;
+            string t = "Happy New Year!";
+
+            Console.WriteLine("There data for i, j, d, f, s, t, b, p are: {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", i, j, d, f, s, t, b, p);
+            Console.ReadLine();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            NullableTest naTest = new NullableTest();
+
+            naTest.ShowNumber();
         }
     }
 }
